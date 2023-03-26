3989113b1a7168350e163a2452c7b96bdf0bb4cd
Peter Abma <charlatans@chello.nl>
2023-03-26 19:11:54 +0200

    feature maart

--- src/main/java/Tester.java
+++ src/main/java/Tester.java
@@ -11,7 +11,7 @@ class Tester {
 	 * sequential 1 to 50 Magic = 3 gives only max +1 allowed from number taken
 	 */
 
-	static int Magic = 17;//it is ok 2
+	static int Magic = 17;//it is ok 2 feature maart
 
 	public static void main(String[] args) {
 		MijnTabel lotje = new MijnTabel();
